111aec83197320de21be431b6a35562e263b904a
Anmol Jha <anmoljha080@gmail.com>
2025-04-30 16:18:10 +0530

    CORS Error Resolved

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const router = require("./routes");
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
